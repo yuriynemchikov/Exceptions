f92d5469d393ef4d87921bebb0b0d3d7d81c52ae
yuriynemcikov <yuriy1993@inbox.ru>
2022-09-11 13:25:22 +0300

    Task5 is done.

---
+++ Task5.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        Integer[] ints = {1,2, null,3,4,null};
+        checkArray(ints);
+
+    }
+
+    public static void checkArray(Integer[] arr){
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == null){
+                list.add(i);
+            }
+        }
+        if (list.size()>0){
+            System.out.println(new RuntimeException("Индексы массива, где значение равно null: "));
+            System.out.print(printList(list));
+        }
+    }
+
+    public static List<Integer> printList(List<Integer> list){
+
+        for(int i = 0; i<list.size(); i++){
+            list.get(i);
+        }
+        return list;
+    }
+}
